07683f76ecbe45075c06e47ecb91d2fac25f6ff9
Noah Spencer <noahspencer0522@gmail.com>
2021-09-02 19:48:53 -0500

    commit tree ecde110b76abc8aa8c560619596e4ab79a94c94a

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 function distanceFromHqInBlocks (blockNumber){
     console.log("blockNumber", blockNumber)
-    return blockNumber = 42
-    if 
+    if (blockNumber > 42){
+        return blockNumber - 42
+    } else {
+        return 42 - blockNumber
+    }   
+}
+function distanceFromHqInFeet (blockNumber){
+const distanceInBlocks = distanceFromHqInBlocks (blockNumber)
+return distanceInBlocks * 264
+}
+function distanceTravelledInFeet(block1, block2){
+    let blockToBlock 
+    if (block2 >= block1){
+        blockToBlock = block2 - block1
+    }
+    else{
+       blockToBlock = block1 - block2
+    }
+return (264 * blockToBlock)
+}
+function calculatesFarePrice (start, destination){
+    let feetTravelled = distanceTravelledInFeet (start, destination)
+    if (feetTravelled <= 400){
+        return 0
+
+    } else if (feetTravelled > 400 && feetTravelled < 2000){
+        return 2.56
+    } else if (feetTravelled >2000 && feetTravelled <2500){
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+
+
 }
